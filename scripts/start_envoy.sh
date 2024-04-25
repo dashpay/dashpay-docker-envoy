7a5440dd731ec83230b8df7580743f21aa4bb4dc
Ivan Shumkov <ivan@shumkov.ru>
2024-04-25 21:01:02 +0700

    feat: configure log level

--- scripts/start_envoy.sh
+++ scripts/start_envoy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec envoy -c /etc/envoy/envoy.yaml --restart-epoch $RESTART_EPOCH
+exec envoy -c /etc/envoy/envoy.yaml --restart-epoch "$RESTART_EPOCH" --log-level "${LOG_LEVEL:-info}"
